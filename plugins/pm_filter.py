2f44ea92109865c788db7b5a91bebb27f9cbbe91
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-06-01 22:24:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -666,8 +666,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         free = 536870912 - monsize
         monsize = get_size(monsize)
         free = get_size(free)
-        z=await query.message.reply_photo(
-        await z.delete(1)
+        await query.message.reply_photo(
             caption=script.STATUS_TXT.format(total, users, chats, monsize, free),
             photo="https://telegra.ph/file/7ac0c39e477b1e0774db1.jpg", 
             reply_markup=reply_markup,
