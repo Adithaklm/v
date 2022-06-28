85e94f28580dca3baed8be154c0778ee86906c95
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-06-28 13:30:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/fcd74771543305b6ba4b3.jpg')
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001573536733').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
