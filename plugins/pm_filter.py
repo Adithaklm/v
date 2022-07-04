b9045e1b75f09115cb11fd909687f4c6eb16c699
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-07-04 22:58:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -580,7 +580,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "admoni":  
         await query.answer("📮 Iғ Yᴏᴜ Nᴏᴛ Gᴇᴛ Yᴏᴜʀ Rᴇǫᴜᴇsᴛ Mᴏᴠɪᴇ Pʟᴇᴀsᴇ Mᴇɴᴛɪᴏɴ Aᴅᴍɪɴ\n\n1⃣ @admin < movie name >\n\n2⃣ Cʜᴇᴄᴋ Hᴇʟᴘᴇʀ Bᴏᴛ Aɴᴅ Rᴇᴘᴏʀᴛ Issᴜᴇ Bᴏᴛ Lɪɴᴋ Gɪᴠᴇɴ Bᴇʟᴏᴡ\n\n©CɪɴᴇᴍᴀCᴏᴍᴘᴀɴʏ", show_alert=True)   
-    
+
+    elif query.data == "tipsto":  
+        await query.answer(" ⛔️ ᴛɪᴘs ғᴏʀ ᴅᴏᴡɴʟᴏᴀᴅ\n\nɪғ ʏᴏᴜ ɢᴇᴛ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴍᴏᴠɪᴇ ғʀᴏᴍ ʙᴏᴛ sᴛᴏʀᴇ ᴛʜᴇ ᴀɴʏᴡʜᴇʀᴇ\n\nᴛʜᴇ ᴍᴏᴠɪᴇ ғɪʟᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇ ᴀғᴛᴇʀ 𝟻 ʜᴏᴜʀs ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ\n\n© ᴄɪɴᴇᴍᴀᴄᴏᴍᴘᴀɴʏ ", show_alert=True)   
+
 
     elif query.data == "autofilter":
         buttons = [[
